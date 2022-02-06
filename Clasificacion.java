0c1f4386328d4e862266b32e7dbe4260358a39c7
carloscesur <carlos.s233008@cesurformacion.com>
2022-02-06 01:38:51 +0100

    Comentando código Rafa

--- Clasificacion.java
+++ Clasificacion.java
@@ -1,33 +1,48 @@
 public class Clasificacion {
 
-    //Pts - Puntos, V - Victorias, E - Empates, D - Derrotas, GF - Goles a favor, GC - Goles en contra.
-    private Liga liga;
-    private EquipoClasificacion[] clasificacionEquipos;
+    private EquipoClasificacion[] clasificacionEquipos; //Trae el array con los datos de cada equipo en la clasificación
+    private int numeroClasificaion; //Número clasificación equipo
 
-    public Clasificacion(Liga liga){
+
+
+
+
+
+
+
+
+
+
+
+    //CÓDIGO DE RAFA
+
+    //private int numeroClasificacion;
+    //private String nombreClasificacion;
+    //private EquipoClasificacion[] clasificacionEquipos;
+    //private Liga liga;
+
+    //public Clasificacion(Liga liga){
+
         //Sacamos los equipos de la liga
-        this.liga=liga;
-        Equipo[] lista = this.liga.getEquipos();
-        this.clasificacionEquipos =  new EquipoClasificacion[lista.length];
+        //this.liga=liga;
+        //Equipo[] lista = this.liga.getEquipos();
+        //this.clasificacionEquipos =  new EquipoClasificacion[lista.length];
 
         //Metemos los equipos en la lista de la Clasificacion
-        for (int i=0; i<clasificacionEquipos.length;i++){
-            clasificacionEquipos[i]=new EquipoClasificacion();
-            clasificacionEquipos[i].setEquipo(lista[i]);
-        }
+        //for (int i=0; i<clasificacionEquipos.length;i++){
+            //clasificacionEquipos[i]=new EquipoClasificacion();
+            //clasificacionEquipos[i].setEquipo(lista[i]);
+        //}
 
         //Generamos la clasificacion
-
-        Calendario miCalendario = this.liga.getCalendario();
-        // Saco las jornadas
-        // Jornada[] misJornadas = calendario.getJornadas;
-        //for (Jornada j: misJornadas){
-        //      Partidos[] misPartidos = j.getPartidos();
-         //      for (Partido p: misPartidos){
-        //             if (p.getGolesHome>p.getGolesAway){
-        //
-
-
-    }
+        //Calendario miCalendario = this.liga.getCalendario();
+        //// Saco las jornadas
+        //// Jornada[] misJornadas = calendario.getJornadas;
+        ////for (Jornada j: misJornadas){
+        ////      Partidos[] misPartidos = j.getPartidos();
+        ////      for (Partido p: misPartidos){
+        ////             if (p.getGolesHome>p.getGolesAway){
+        ////
+    //}
 
 }
