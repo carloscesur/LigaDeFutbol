ffcbd44e4765e952de11deedff02c990bf27044c
Juan Antonio <94042645+Manssanito@users.noreply.github.com>
2022-03-02 19:27:22 +0100

    Update Jornada.java

--- Jornada.java
+++ Jornada.java
@@ -1,56 +1,38 @@
+import java.util.Arrays;
+
 public class Jornada {
+	
+	private Partido[] partidos;
+	private boolean terminada=false;
 
-     private Partido[] partidos;
-     private Equipo[] equiposEmparejados;
+	public Partido[] getPartidos() {
+		return partidos;
+	}
 
-     public Jornada(Equipo[] equipos){
+	public void setPartidos(Partido[] partidos) {
+		this.partidos = partidos;
+	}
+	
 
-          this.equiposEmparejados = equipos;
-          this.partidos = generarPartidos(equiposEmparejados);
-     }
+	public boolean isTerminada() {
+		return terminada;
+	}
 
-     private Partido[] generarPartidos(Equipo[] equiposEmparejados){
+	public void setTerminada(boolean terminada) {
+		this.terminada = terminada;
+	}
+	
+	public void terminar() {
+		this.terminada= true;
+	}
 
-          Partido[] partidos = new Partido[equiposEmparejados.length/2];
-
-          int indeceArrayPartido =0 ;
-
-          for (int i=0; i<equiposEmparejados.length; i=i+2){
-
-
-
-               if(equiposEmparejados[i]!=null && equiposEmparejados[i+1]!=null){
-                    Partido partido = new Partido();
-                    partido.setEquipoLocal(equiposEmparejados[i]);
-                    partido.setEquipoVisitante(equiposEmparejados[i+1]);
-
-                    partido.golesLocal();
-                    partido.golesVisitane();
-
-
-                    //ME QUEDA PASAR LOS PARTIDOS AL ARRAY DE PARTIDOS CORRECTAMENTE
-                    partidos[indeceArrayPartido]=partido;
-
-                    indeceArrayPartido = indeceArrayPartido+1;
-               }
-          }
-
-          return partidos;
-     }
-
-     //Setters
-     public void setPartidos(Partido[] partidos) {
-          this.partidos = partidos;
-     }
-     public void setEquiposEmparejados(Equipo[] equiposEmparejados) {
-          this.equiposEmparejados = equiposEmparejados;
-     }
-
-     //Getters
-     public Partido[] getPartidos() {
-          return partidos;
-     }
-     public Equipo[] getEquiposEmparejados() {
-          return equiposEmparejados;
-     }
+	@Override
+	public String toString() {
+		String cadena="Jornada: \n";
+		for (int i=0;i<this.partidos.length;i++) {
+			cadena+=partidos[i];
+		}
+		return cadena;
+	}
+	
 }
